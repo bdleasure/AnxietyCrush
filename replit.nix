e45fa0721eb2c4626ff2173c0cf236901c7e2ca4
bdleasure <bdleasure@gmail.com>
2024-11-21 15:54:54 -0600

    Fix: Update Node.js version and add npm to replit.nix

--- replit.nix
+++ replit.nix
@@ -1,7 +1,8 @@
 { pkgs }: {
     deps = [
-        pkgs.nodejs-18_x
+        pkgs.nodejs-20_x
         pkgs.nodePackages.typescript-language-server
+        pkgs.nodePackages.npm
         pkgs.yarn
     ];
 }
