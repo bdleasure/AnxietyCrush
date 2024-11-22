fea2f0a62a9ce4c0b110a111c0e28b33d5d52ae7
bdleasure <bdleasure@gmail.com>
2024-11-21 19:29:44 -0600

    Update Expo dependencies and Babel configuration for SDK 52

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,11 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
       '@babel/plugin-transform-optional-chaining',
       '@babel/plugin-transform-nullish-coalescing-operator',
-      '@babel/plugin-transform-class-properties',
       'react-native-reanimated/plugin',
     ],
   };
